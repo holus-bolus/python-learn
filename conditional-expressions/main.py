5a1c0d035565b06ac902cc4ab0d315fb08e39488
holus-bolus <ustinovoleksij@gmail.com>
2024-08-26 15:42:26 +0300

    Added the string indexes

---
+++ conditional-expressions/main.py
@@ -0,0 +1,6 @@
+name = input('Enter your name: ')
+
+# result = len(name)
+result = name.find('O')
+
+print('Your name has', result, 'characters')
---
+++ logical-operators/main.py
@@ -0,0 +1,9 @@
+# or, and, not
+
+temp = 25
+is_raining = False
+
+if temp > 26 or temp < 0 or is_raining:
+    print('The outdoor event is cancelled')
+else:
+    print('The outdoor event is still valid')
