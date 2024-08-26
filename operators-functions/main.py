e42a01d3eb8096910be0cd9fadc4d6f5bf989ac5
holus-bolus <ustinovoleksij@gmail.com>
2024-08-26 08:48:57 +0300

    Added math functions

---
+++ operators-functions/main.py
@@ -0,0 +1,23 @@
+import math
+friends = 0
+
+friends += 10
+
+# friends /=2
+# friends **=2
+remainder = friends % 3
+
+print(f" the amount of friends is {friends}")
+print(remainder)
+
+# Math related functions
+
+x = 3.14
+y = 4
+z = 5
+
+result = round(x)
+
+print(f"the result is {result}")
+print(math.pi)
+print(math.e)
